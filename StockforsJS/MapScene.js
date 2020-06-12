44e08b0608588d26cc6c3aff63fab493cb243593
oanku012 <54891306+oanku012@users.noreply.github.com>
2020-06-12 20:32:47 +0300

    Testailin pelaajan spawnauskohdan asettamista rakennuksesta pois tullessa

--- StockforsJS/MapScene.js
+++ StockforsJS/MapScene.js
@@ -1,7 +1,5 @@
-class MapScene extends Phaser.Scene 
-{
-    constructor(SceneKey)
-    {
+class MapScene extends Phaser.Scene {
+    constructor(SceneKey) {
 
         super(SceneKey);
 
@@ -16,27 +14,47 @@ class MapScene extends Phaser.Scene
         this.speed = 5;
         this.movementVector = new Phaser.Math.Vector2();
         this.destination = new Phaser.Math.Vector2();
+        this.readyToMove;
 
         this.buildings = {};
 
         this.sceneToOpen;
-        
+
+        this.startingPoint = {
+            x: 0,
+            y: 0
+        }
+
     }
 
-    preload ()
-    {
+    //This is called when starting scene with this.scene.start
+    init(startingPointX, startingPointY) {
+
         
+        //if (isNaN(startingPointX) == false && isNaN(startingPointY) == false) {
+            
+            console.log('Hello');
+            this.startingPoint.x = startingPointX;
+            this.startingPoint.y = startingPointY;
+        //}
+
     }
 
-    create (startingPointX, startingPointY)
-    {
+    preload() {
+
+    }
+
+    create() {
+
         console.log(this.scene.key);
+
+        this.readyToMove = false;
 
         this.sceneToOpen = null;
 
         this.matter.world.setBounds();
 
-        this.player = this.matter.add.sprite(startingPointX, startingPointY, 'player').setBounce(0).setFixedRotation().setFriction(20, 0).setIgnoreGravity(true);
+        this.player = this.matter.add.sprite(this.startingPoint.x, this.startingPoint.y, 'player').setBounce(0).setFixedRotation().setFriction(20, 0).setIgnoreGravity(true);
         this.player.setDepth(this.player.y);
 
         this.arrowKeys = this.input.keyboard.createCursorKeys();
@@ -47,41 +65,39 @@ class MapScene extends Phaser.Scene
 
         //Makes it so that the pointer world position is updated when camera moves
         this.input.setPollAlways();
-        
+
         this.CreateAnimations();
         this.BuildingsInitialize();
         this.InitializeCamera();
+        this.MovementInitialize();
 
         //Movement is initialized with a slight delay so that player clicking the button to return outside won't trigger movement
-        this.time.delayedCall(100, this.MovementInitialize, null, this)
-        
-        
-        
-        
+        this.time.delayedCall(200, function () { this.readyToMove = true; }, null, this)
+
+
+
+
     }
 
-    update ()
-    {
-        
+    update() {
+
         this.MovementUpdate();
-      
-        if(this.player.body.speed > 0)
-        {
+
+        if (this.player.body.speed > 0) {
             this.player.setDepth(this.player.y);
         }
 
-       
+
     }
 
-    
 
-    BuildingsInitialize()
-    {
-        
+
+    BuildingsInitialize() {
+
     }
 
-    MovementInitialize(){
-    
+    MovementInitialize() {
+
         //Make player move in direction of mouse click
         /*this.input.on('pointerdown', function ()
         {
@@ -89,247 +105,232 @@ class MapScene extends Phaser.Scene
             
         }, this);*/
 
-        this.matter.world.on('collisionstart', function(event, bodyA, bodyB)
-        {
+        this.matter.world.on('collisionstart', function (event, bodyA, bodyB) {
             //Checks if either of the colliding objects contain the openScene function
-            if(bodyB.gameObject != null)
-            {
-                if(typeof bodyB.gameObject.openScene === 'function')
-                {
+            if (bodyB.gameObject != null) {
+                if (typeof bodyB.gameObject.openScene === 'function') {
                     bodyB.gameObject.openScene();
                 }
             }
-            else if(bodyA.gameObject != null)
-            {
-                if(typeof bodyA.gameObject.openScene === 'function')
-                {
+            else if (bodyA.gameObject != null) {
+                if (typeof bodyA.gameObject.openScene === 'function') {
                     bodyA.gameObject.openScene();
                 }
             }
 
-            
+
         });
-        
+
     }
-    
-    
-    MovementUpdate(){
-        
-        
-
-        //Movement with WASD and arrow keys
-        if (this.arrowKeys.left.isDown || this.wasdKeys.A.isDown)
-        {
-            this.player.setVelocityX(-this.speed);
-            this.movingOnPath = false;
-        }
-        else if (this.arrowKeys.right.isDown || this.wasdKeys.D.isDown)
-        {
-            this.player.setVelocityX(this.speed);
-            this.movingOnPath = false;
-    
-        }
-        else if(this.movingOnPath == false)
-        {
-            
-            this.player.setVelocityX(0);
-            
-        }
-    
-        if (this.arrowKeys.up.isDown || this.wasdKeys.W.isDown)
-        {
-            this.player.setVelocityY(-this.speed);
-            this.movingOnPath = false;
-    
-        }
-        else if (this.arrowKeys.down.isDown || this.wasdKeys.S.isDown)
-        {
-            this.player.setVelocityY(this.speed);
-            this.movingOnPath = false;
-    
-        }
-        else if(this.movingOnPath == false)
-        {
-            this.player.setVelocityY(0);
-        }
-        else if (this.movingOnPath == true)
-        {
-            let distanceToDestination;
-
-            //Check the distance between the player and the destination player clicked
-            distanceToDestination = this.CheckDistance(this.player, this.destination);
 
 
-            if (distanceToDestination < 4)
-            {
-                this.player.setVelocity(0,0);
-    
+    MovementUpdate() {
+
+        if (this.readyToMove == true) {
+
+            //Movement with WASD and arrow keys
+
+            if (this.arrowKeys.left.isDown || this.wasdKeys.A.isDown) {
+
+                this.player.setVelocityX(-this.speed);
+
                 this.movingOnPath = false;
-    
-                //This is just to let the player move using keys after moving to a position with the mouse
-                //this.destination.x = -1000;
-                //this.destination.y = -1000;
+
             }
-    
-            
+            else if (this.arrowKeys.right.isDown || this.wasdKeys.D.isDown) {
+                this.player.setVelocityX(this.speed);
+                this.movingOnPath = false;
+
+
+            }
+
+            else if (this.movingOnPath == false) {
+
+
+                this.player.setVelocityX(0);
+
+
+            }
+
+
+            if (this.arrowKeys.up.isDown || this.wasdKeys.W.isDown) {
+
+                this.player.setVelocityY(-this.speed);
+
+                this.movingOnPath = false;
+
+
+            }
+
+            else if (this.arrowKeys.down.isDown || this.wasdKeys.S.isDown) {
+
+                this.player.setVelocityY(this.speed);
+                this.movingOnPath = false;
+
+            }
+            else if (this.movingOnPath == false) {
+                this.player.setVelocityY(0);
+            }
+            else if (this.movingOnPath == true) {
+                let distanceToDestination;
+
+                //Check the distance between the player and the destination player clicked
+                distanceToDestination = this.CheckDistance(this.player, this.destination);
+
+
+                if (distanceToDestination < 4) {
+                    this.player.setVelocity(0, 0);
+
+                    this.movingOnPath = false;
+
+                    //This is just to let the player move using keys after moving to a position with the mouse
+                    //this.destination.x = -1000;
+                    //this.destination.y = -1000;
+                }
+
+
+            }
+
+
+            if (this.pointer.isDown == true) {
+                this.destination.x = this.pointer.worldX;
+                this.destination.y = this.pointer.worldY;
+
+                if (this.CheckDistance(this.player, this.destination) > 30) {
+
+                    this.movementVector.x = this.pointer.worldX - this.player.x;
+                    this.movementVector.y = this.pointer.worldY - this.player.y;
+
+                    this.movementVector.setLength(this.speed);
+
+                    this.player.setVelocity(this.movementVector.x, this.movementVector.y);
+
+                    this.movingOnPath = true;
+                }
+            }
+
+            //Apply the correct animations to player based on direction of movement
+            if (this.player.body.speed > 0) {
+                const obliqueThreshold = 2 / 10;
+                const straightThreshold = 8 / 10;
+
+                console.log(this.player.body.velocity);
+
+                if (this.player.body.velocity.x > (this.speed * (straightThreshold))) {
+
+                    console.log('Moving right');
+
+                    this.player.anims.play('right', true);
+                }
+                else if (this.player.body.velocity.y > (this.speed) * (obliqueThreshold) && this.player.body.velocity.x > (this.speed * obliqueThreshold)) {
+                    console.log('Moving downright');
+
+                    this.player.anims.play('downright', true);
+                }
+                else if (this.player.body.velocity.y > (this.speed * (straightThreshold))) {
+                    console.log('Moving down');
+
+                    this.player.anims.play('down', true);
+                }
+                else if (this.player.body.velocity.y > (this.speed) * (obliqueThreshold) && this.player.body.velocity.x < (-this.speed * obliqueThreshold)) {
+                    console.log('Moving downleft');
+
+                    this.player.anims.play('downleft', true);
+                }
+                else if (this.player.body.velocity.x < (-this.speed * (straightThreshold))) {
+                    console.log('Moving left');
+
+                    this.player.anims.play('left', true);
+                }
+                else if (this.player.body.velocity.y < (-this.speed) * (obliqueThreshold) && this.player.body.velocity.x < (-this.speed * obliqueThreshold)) {
+                    console.log('Moving upleft');
+
+                    this.player.anims.play('upleft', true);
+                }
+                else if (this.player.body.velocity.y < (-this.speed * (straightThreshold))) {
+                    console.log('Moving up');
+
+                    this.player.anims.play('up', true);
+                }
+                else if (this.player.body.velocity.y < (-this.speed) * (obliqueThreshold) && this.player.body.velocity.x > (this.speed * obliqueThreshold)) {
+                    console.log('Moving upright');
+
+                    this.player.anims.play('upright', true);
+                }
+
+            }
+
         }
 
-
-        if(this.pointer.isDown == true)
-        {
-            this.destination.x = this.pointer.worldX;
-            this.destination.y = this.pointer.worldY;
-
-            if(this.CheckDistance(this.player, this.destination)>30)
-            {
-
-                this.movementVector.x = this.pointer.worldX-this.player.x;
-                this.movementVector.y = this.pointer.worldY-this.player.y;
-
-                this.movementVector.setLength(this.speed);
-
-                this.player.setVelocity(this.movementVector.x, this.movementVector.y);
-
-                this.movingOnPath = true;
-            }
-        }
-
-        //Apply the correct animations to player based on direction of movement
-        if(this.player.body.speed>0)
-        {
-            const obliqueThreshold = 2/10;
-            const straightThreshold = 8/10;
-
-            console.log(this.player.body.velocity);
-
-            if(this.player.body.velocity.x>(this.speed*(straightThreshold)))
-            {
-            
-                console.log('Moving right');
-                
-                this.player.anims.play('right', true);
-            }
-            else if(this.player.body.velocity.y>(this.speed)*(obliqueThreshold) && this.player.body.velocity.x>(this.speed*obliqueThreshold))
-            {
-                console.log('Moving downright');
-
-                this.player.anims.play('downright', true);
-            }
-            else if(this.player.body.velocity.y>(this.speed*(straightThreshold)))
-            {
-                console.log('Moving down');
-
-                this.player.anims.play('down', true);
-            }
-            else if(this.player.body.velocity.y>(this.speed)*(obliqueThreshold) && this.player.body.velocity.x<(-this.speed*obliqueThreshold))
-            {
-                console.log('Moving downleft');
-
-                this.player.anims.play('downleft', true);
-            }
-            else if(this.player.body.velocity.x<(-this.speed*(straightThreshold)))
-            {
-                console.log('Moving left');
-
-                this.player.anims.play('left', true);
-            }
-            else if(this.player.body.velocity.y<(-this.speed)*(obliqueThreshold) && this.player.body.velocity.x<(-this.speed*obliqueThreshold))
-            {
-                console.log('Moving upleft');
-
-                this.player.anims.play('upleft', true);
-            }
-            else if(this.player.body.velocity.y<(-this.speed*(straightThreshold)))
-            {
-                console.log('Moving up');
-
-                this.player.anims.play('up', true);
-            }
-            else if(this.player.body.velocity.y<(-this.speed)*(obliqueThreshold) && this.player.body.velocity.x>(this.speed*obliqueThreshold))
-            {
-                console.log('Moving upright');
-
-                this.player.anims.play('upright', true);
-            }
-            
-        }
-        
 
     }
 
     //Check distance between two objects
-    CheckDistance(sourceObject, destinationObject)
-    {
+    CheckDistance(sourceObject, destinationObject) {
         let distance = Phaser.Math.Distance.Between(sourceObject.x, sourceObject.y, destinationObject.x, destinationObject.y);
 
         return distance;
     }
 
-    InitializeCamera()
-    {
-    let camera = this.cameras.main;
+    InitializeCamera() {
+        let camera = this.cameras.main;
 
-    let maxZoom = 3;
-    let minZoom = 1;
+        let maxZoom = 3;
+        let minZoom = 1;
 
-    camera.startFollow(this.player, true, 0.08, 0.08);
+        camera.startFollow(this.player, true, 0.08, 0.08);
 
-    this.input.on('wheel', function (pointer, gameObjects, deltaX, deltaY, deltaZ){
-        
-        //Zooms camera in and out when using scroll wheel, zoom is stopped at min and max values
-        if((camera.zoom - deltaY*0.05) > minZoom)
-        {
-            if((camera.zoom - deltaY*0.05) < maxZoom)
-            {
-                camera.setZoom(camera.zoom - deltaY*0.05); 
+        this.input.on('wheel', function (pointer, gameObjects, deltaX, deltaY, deltaZ) {
+
+            //Zooms camera in and out when using scroll wheel, zoom is stopped at min and max values
+            if ((camera.zoom - deltaY * 0.05) > minZoom) {
+                if ((camera.zoom - deltaY * 0.05) < maxZoom) {
+                    camera.setZoom(camera.zoom - deltaY * 0.05);
+                }
+
             }
-            
-        }
-        else
-        {
-            camera.setZoom(minZoom);
+            else {
+                camera.setZoom(minZoom);
 
-        }        
+            }
 
-        
 
-        
-    });
+
+
+        });
     }
 
-    createButton (posX, posY) 
-    {
+    createButton(posX, posY) {
         // Button
         let buttonBG = this.add.image(0, 0, 'buttonBG');
         let buttonText = this.add.image(0, 0, 'buttonText');
-    
-        let button = this.add.container(posX, posY, [ buttonBG, buttonText ]);
+
+        let button = this.add.container(posX, posY, [buttonBG, buttonText]);
         button.setSize(buttonBG.width, buttonBG.height);
         button.setInteractive();
-        
+
         button.on('pointerover', function () {
-    
+
             buttonBG.setTint(0x44ff44);
-    
+
         });
-    
+
         button.on('pointerout', function () {
-    
+
             buttonBG.clearTint();
-    
+
         });
-    
+
         button.on('pointerdown', function (event) {
             // ...
             this.scene.start('MenuScene');
-          }, this);
-    
-        
+        }, this);
+
+
     }
 
-    CreateAnimations()
-    {
-        
+    CreateAnimations() {
+
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
@@ -346,7 +347,7 @@ class MapScene extends Phaser.Scene
 
         this.anims.create({
             key: 'down',
-            frames: [{key: 'player', frame: 4 }],
+            frames: [{ key: 'player', frame: 4 }],
             frameRate: 10,
             repeat: -1
         });
@@ -385,7 +386,7 @@ class MapScene extends Phaser.Scene
             frameRate: 10,
             repeat: -1
         });
-    
+
     }
 
 
--- StockforsJS/StockforsScene.js
+++ StockforsJS/StockforsScene.js
@@ -1,13 +1,12 @@
-class StockforsScene extends MapScene
-{
-    constructor()
-    {
+class StockforsScene extends MapScene {
+    constructor() {
+
         super('StockforsScene');
+
 
     }
 
-    preload()
-    {
+    preload() {
         this.load.image('map', 'Assets/images/map/Red Bank.png');
         this.load.spritesheet('player', 'Assets/images/character/dude.png', { frameWidth: 32, frameHeight: 48 });
         this.load.image('buttonBG', 'Assets/images/menu/button-bg.png');
@@ -15,49 +14,47 @@ class StockforsScene extends MapScene
         this.load.image('menuBG', 'Assets/images/menu/menu-bg.png');
 
         this.load.image('PatruunanTalo', 'Assets/images/map/Buildings/rem_0002');
-        this.load.image('PakkausMuseo','Assets/images/map/Buildings/rem_0005');
+        this.load.image('PakkausMuseo', 'Assets/images/map/Buildings/rem_0005');
         this.load.image('exitButton', 'Assets/images/menu/exit-button.png');
 
         this.load.image('Nuoli', 'Assets/images/map/arrowSign');
     }
 
     //Use parameters when starting this scene from another scene to set the position of the player
-    create(startingPointX, startingPointY)
-    {
-        //Check if parameters are not numbers and if so give them the default values
-        if(isNaN(startingPointX) && isNaN(startingPointY))
-        {
-            startingPointX = 200;
-            startingPointY = 300;
+    create() {
+
+        //Check if initialized parameters are not numbers and if so give them default values
+        if (isNaN(this.startingPoint.x) && isNaN(this.startingPoint.y)) {
+            this.startingPoint.x = 200;
+            this.startingPoint.y = 300;
         }
 
         this.add.image(400, 300, 'map').setDepth(0);
-        this.add.text(300, 40, "Stockfors", {font: "40px Arial", fill: "yellow"});
-        
-        
-        super.create(startingPointX, startingPointY);
+        this.add.text(300, 40, "Stockfors", { font: "40px Arial", fill: "yellow" });
+
+
+        super.create();
         this.createButton(200, 200);
 
     }
 
-    BuildingsInitialize()
-    {
+    BuildingsInitialize() {
         super.BuildingsInitialize();
 
         const thisScene = this.scene;
 
         this.buildings.PatruunanTalo = this.matter.add.image(600, 400, 'PatruunanTalo').setScale(0.3).setStatic(true);
-        this.buildings.PatruunanTalo.openScene = function(){
+        this.buildings.PatruunanTalo.openScene = function () {
             thisScene.start('PatruunaScene');
         };
-        
+
         this.buildings.PakkausMuseo = this.matter.add.image(800, 200, 'PakkausMuseo').setScale(0.3).setStatic(true);
-        this.buildings.PakkausMuseo.openScene = function(){
+        this.buildings.PakkausMuseo.openScene = function () {
             thisScene.start('PakkausMuseoScene');
         };
-        
+
         this.buildings.KirkkoTie = this.matter.add.image(1400, 500, 'Nuoli').setScale(0.1).setStatic(true);
-        this.buildings.KirkkoTie.openScene = function(){
+        this.buildings.KirkkoTie.openScene = function () {
             thisScene.start('KirkkoScene');
         };
     }
